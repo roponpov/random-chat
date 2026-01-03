33ccedcdbc36f5e88d2464e4cdba5079d87e6190
Pov Ropon <19993@kh2-acledabank.local>
2026-01-03 07:57:29 +0700

    Update icons

--- lib/core/constants/app_icon_constant.dart
+++ lib/core/constants/app_icon_constant.dart
@@ -1,7 +1,7 @@
 class AppIconConstant {
   AppIconConstant._();
 
-  static const String _defaultPath = 'lib/assets/icons';
+  static const String _defaultPath = 'lib/assets/icons369';
   static const String chatIcon = '$_defaultPath/chat_icon.png';
   static const String secureIcon = '$_defaultPath/secure_icon.png';
 
